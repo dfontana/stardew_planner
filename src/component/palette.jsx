154ea08a2fe1203318869c559fa47481a74de5d7
dfontana <dyfontana@gmail.com>
2018-05-23 19:47:32 -0400

    Made expandable palettes, fixed layout to be one page

--- src/component/palette.jsx
+++ src/component/palette.jsx
@@ -3,15 +3,52 @@ import React, { Component } from 'react'
 class Palette extends Component {
   constructor(props) {
     super(props)
-    this.style = {
-      backgroundColor: 'yellow',
-      gridArea: props.location
+    this.state = {
+      isOpen: false
     }
   }
 
+  handleOpen = (e) => {
+    this.setState({isOpen: !this.state.isOpen})
+  }
+
   render() {
+    let isLeft = this.props.location == 'palette_left';
+    let W = '500px'
+    let H = '100px'
+
+    let containerStyle = {
+      backgroundColor: 'red',
+      gridArea: this.props.location,
+      placeSelf: isLeft ? 'center start' : 'start center',
+
+      display: 'flex',
+      flexDirection: isLeft ? 'row' : 'column',
+      justifyContent: 'start',
+      alignItems: 'center'
+    }
+
+    let buttonStyle = {
+      width: isLeft ? '10px' : '20px',
+      height: isLeft ? '20px' : '10px',
+      backgroundColor: 'yellow',
+      cursor: 'pointer',
+      zIndex: 99
+    }
+
+    let paletteStyle = {
+      backgroundColor: 'orange',
+      width: isLeft ? H : W,
+      height: isLeft ? W : H,
+      display: this.state.isOpen ? 'block' : 'none',
+      zIndex: 99
+    }
+
     return (
-      <div style={this.style}>Suh dude.</div>
+      <div style={containerStyle}>
+        <div style={paletteStyle}></div>
+        <div style={buttonStyle} onClick={this.handleOpen}></div>
+      </div>
     )
   }
 }
